722b2dec52e41d524eae6c67ebd26dcb6f907e42
Olivier Delbruyere <olivier.delbruyere@hotmail.com>
2016-09-14 18:26:32 +0200

    refacto

---
+++ Service/AdminSidebarHandler.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Dywee\FaqBundle\Service;
+
+use Symfony\Component\Routing\Router;
+
+class AdminSidebarHandler
+{
+
+    private $router;
+
+    public function __construct(Router $router)
+    {
+        $this->router = $router;
+    }
+
+    public function getSideBarMenuElement()
+    {
+        $menu = array(
+            'key' => 'faq',
+            'icon' => 'fa fa-newspaper-o',
+            'label' => 'faq.sidebar.label',
+            'children' => array(
+                array(
+                    'icon' => 'fa fa-list-alt',
+                    'label' => 'faq.sidebar.dashboard',
+                    'route' => $this->router->generate('faq_homepage')
+                ),
+            )
+        );
+
+        return $menu;
+    }
+}
